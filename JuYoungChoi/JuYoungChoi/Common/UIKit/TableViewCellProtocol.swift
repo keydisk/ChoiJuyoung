2722162b19a4683c37aab61b15ae168960655ad8
JuYoung choi <>
2018-04-23 15:17:12 +0900

    기본 로직 추가

---
+++ JuYoungChoi/JuYoungChoi/Common/UIKit/TableViewCellProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCellExtension.swift
+//  ChoiJuYoung
+//
+//  Created by JuYoung choi on 2018. 4. 18..
+//  Copyright © 2018년 JuYoung choi. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+protocol UITableViewCellData {
+    
+    var cellData: JSON! {get set}
+}
